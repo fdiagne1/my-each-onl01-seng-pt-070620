d9a171d48756e55ce877052382cc885c51d5a118
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-16 19:09:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,32 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+    
+  while i < array.length
+    yield(collection [i])
+    i = i + 1
 end
+  collection
+end
+end
+
+
+# def hello_t(array)
+#   if block_given?
+#     i = 0
+  
+#   while i < array.length
+#     yield(array [i])
+#     i = i + 1
+#   end
+#     array
+#   else
+#     puts "Hey! No block was given!"
+#   end
+# end
+
+# hello_t(["Tim", "Tom", "Jim"]) do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
